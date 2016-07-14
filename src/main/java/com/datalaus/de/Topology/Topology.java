419480438895ac771cece2b2980ae255dda456b5
dataapplabTerm5 <dataapplabTerm5@gmail.com>
2016-07-13 23:44:54 -0700

    added kafkabolt

--- src/main/java/com/datalaus/de/Topology/Topology.java
+++ src/main/java/com/datalaus/de/Topology/Topology.java
@@ -1,6 +1,8 @@
 package com.datalaus.de.Topology;
 
 import java.io.Serializable;
+import java.util.Properties;
+import java.util.UUID;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
@@ -9,6 +11,12 @@ import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.utils.Utils;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import storm.kafka.BrokerHosts;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +32,29 @@ public class Topology implements Serializable {
 	public static final void main(final String[] args) {
 		try {
 			
+			Properties topologyConfig = null;
 			final Config config = new Config();
 			config.setMessageTimeoutSecs(20);
 
 			TopologyBuilder topologyBuilder = new TopologyBuilder();
-			topologyBuilder.setSpout("twitterspout", new TwitterSpout(),1);
-			topologyBuilder.setBolt("DisplayBolt", new DisplayBolt()).shuffleGrouping("twitterspout");
-			topologyBuilder.setBolt("WordSplitterBolt", new WordSplitterBolt(5)).shuffleGrouping("twitterspout");
+			
+			String configFileLocation = "config.properties";
+		    topologyConfig = new Properties();
+		    topologyConfig.load(ClassLoader.getSystemResourceAsStream(configFileLocation));
+		    String zkConnString = topologyConfig.getProperty("zookeeper");
+		    String topicName = topologyConfig.getProperty("topic");
+			BrokerHosts hosts = new ZkHosts(zkConnString);
+			SpoutConfig spoutConfig = new SpoutConfig(hosts, topicName, "/" + topicName, UUID.randomUUID().toString());
+			spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+			KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+
+		    // attach the tweet spout to the topology - parallelism of 1
+			topologyBuilder.setSpout("kafka-tweet-spout", kafkaSpout, 1);
+			
+			
+			//topologyBuilder.setBolt("DisplayBolt", new DisplayBolt()).shuffleGrouping("twitterspout");
+			topologyBuilder.setBolt("tweet-original", new TweetKafkabolt(),1).shuffleGrouping("kafka-tweet-spout");
+			topologyBuilder.setBolt("WordSplitterBolt", new WordSplitterBolt(5)).shuffleGrouping("tweet-original");
 			topologyBuilder.setBolt("WordCounterBolt", new WordCounterBolt(10, 5 * 60, 50)).shuffleGrouping("WordSplitterBolt");
 			
 			//Submit it to the cluster or  locally
---
+++ src/main/java/com/datalaus/de/bolts/TweetKafkabolt.java
@@ -0,0 +1,53 @@
+package com.datalaus.de.bolts;
+
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import twitter4j.Status;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+public class TweetKafkabolt extends BaseRichBolt{
+    OutputCollector _collector;
+    LinkedBlockingQueue<Status> statusqueue = new LinkedBlockingQueue<Status>();
+    private static final Logger logger = LoggerFactory.getLogger(TweetKafkabolt.class);
+    
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        _collector = outputCollector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+    	   statusqueue.offer(tuple);
+    	  String ret = tuple.getString(0);
+    	   _collector.emit(new Values(ret));
+           logger.info("\n\n");
+           logger.info(ret);
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("tweet"));
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+
+}
